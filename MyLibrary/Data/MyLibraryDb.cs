62037de8c6b708cace397e2e7d976b71287aaaad
VladimirRadev <neymaradev@gmail.com>
2020-02-09 14:51:12 +0200

    adding exciting elemenets

---
+++ MyLibrary/Data/MyLibraryDb.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MyLibraryDb : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-9F6IC4T\SQLEXPRESS;
+Initial Catalog=MyLibraryDb
+;Integrated Security=true");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
